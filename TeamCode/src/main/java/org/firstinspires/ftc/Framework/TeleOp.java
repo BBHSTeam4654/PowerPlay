b20c59f991d96175e951a6e47c2fb949f7315d00
laoifeio <laoifeio@gmail.com>
2022-12-05 16:36:27 -0500

    Update TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/Framework/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/Framework/TeleOp.java
@@ -2,3 +2,5 @@ package org.firstinspires.ftc.teamcode.framework;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
