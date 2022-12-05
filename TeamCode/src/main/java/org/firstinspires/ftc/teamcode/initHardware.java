b2bea183c2d7359697ff45093b99a31bfd0a6fee
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2022-12-05 17:17:55 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initHardware.java
@@ -1,3 +1,10 @@
-public class InitHardware {
+package org.firstinspires.ftc.teamcode.framework;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import org.firstinspires.ftc.teamcode.framework.*;
+public class initHardware {
     
 }
