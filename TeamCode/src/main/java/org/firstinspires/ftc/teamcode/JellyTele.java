76a4b23f1472d5295f71769216b04d7f1d373fa6
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2022-12-14 00:28:57 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
@@ -30,7 +30,7 @@ public class JellyTele extends BaseOpMode{
                 driveMode = DriveMode.DRIVE;
             }
             //percision mult
-            double mult = gamepad1.left_bumper ? 0.35 : gamepad1.right_bumper ? 0.7 : 1.0;
+            double mult = gamepad1.left_bumper ? 0.25 : gamepad1.right_bumper ? 0.75 : 1.0;
 
             telemetry.addData("drive mode", driveMode);
             telemetry.addData("precision mode", mult);
@@ -39,17 +39,19 @@ public class JellyTele extends BaseOpMode{
             switch(driveMode){
                 case TANK:{
                     setMotorSpeeds(mult, new double[] {-gamepad1.right_stick_y, -gamepad1.right_stick_y, -gamepad1.left_stick_y, -gamepad1.left_stick_y});
-                    break;
+                    break; 
                 }
                 case DRIVE:{
-                    double pivot = gamepad1.left_stick_x
+                    double pivot = gamepad1.left_stick_x;
                     double y = -gamepad1.left_stick_y;
 
                     setMotorSpeeds(mult,new double[]{y-pivot,y-pivot,y+pivot,y+pivot})
                     break;
                 }
                 case MECANUM:{
+                    //Right stick pivot
                     double pivot = gamepad1.right_stick_x;
+                    //Strafing
                     double mX = gamepad1.left_stick_x;
                     double mY = gamepad1.left_stick_y;
                     setMotorSpeeds(mult,new double[]){mY - mX - pivot, mY + mX - pivot, mY + mX + pivot, mY - mX + pivot}
@@ -57,8 +59,26 @@ public class JellyTele extends BaseOpMode{
                     break;
                 }
             }
-            
-
+        }
+        //Multi
+        protected void setMotorSpeeds(double mult, double[] powers) {
+            for (int i = 0; i < 4; i++) {
+                powers[i] = powers[i] * mult;
+            }
+    
+            double max = Math.max(Math.max(Math.abs(powers[0]), Math.abs(powers[1])), Math.max(Math.abs(powers[2]), Math.abs(powers[3])));
+            double scale = Math.abs(1 / max);
+            if (scale > 1) {
+                scale = 1;
+            }
+    
+            for (int i = 0; i < 4; i++) {
+                powers[i] *= scale;
+            }
+    
+            for (int i = 0; i < 4; i++) {
+                motors[i].setPower(powers[i]);
+            }
         }
     }
 }
