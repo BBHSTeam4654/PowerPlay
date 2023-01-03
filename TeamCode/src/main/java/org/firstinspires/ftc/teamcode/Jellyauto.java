b361f9ae9c9ce4430f329d547c8b1078d1027cb3
Moocow <moocow.gg@gmail.com>
2023-01-03 15:08:28 -0500

    update time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jellyauto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jellyauto.java
@@ -22,7 +22,6 @@
  package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Auto.AprilTagDetectionPipeline;
