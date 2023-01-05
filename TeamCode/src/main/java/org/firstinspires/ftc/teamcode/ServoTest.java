a36b75b01b2527d6d60097671a13c7d3102c971e
laoifeio <laoifeio@gmail.com>
2023-01-05 16:57:44 -0500

    Update ServoTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -14,7 +14,11 @@ public class ServoTest extends LinearOpMode{
 
 
         while(opModeIsActive()){
-            
+            if (gamepad1.left_bumper){
+                clawServo.setPosition(0.35);
+            } else if (gamepad1.right_bumper){
+                clawServo.setPosition(0);
+            }
         }
     }
 }
