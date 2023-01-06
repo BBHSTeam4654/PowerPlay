8dc9b225e2f325bbfe7ac06700e9ff0483d9ca5b
matthewlaterza <102398918+matthewlaterza@users.noreply.github.com>
2023-01-06 15:03:47 -0500

    Update SlidesTest2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlidesTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlidesTest2.java
@@ -24,29 +24,22 @@ public class SlidesTest2 extends LinearOpMode {
             telemetry.update();
 
             if (gamepad1.left_stick_y != 0) {
-
-
-            while (gamepad1.left_stick_y != 0) {
-
                 frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                 frontLeftMotor.setPower(gamepad1.left_stick_y);
             }
-            else if (gamepad1.a) {
+            if (gamepad1.a) {
                 frontLeftMotor.setTargetPosition(-4000);
                 frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 frontLeftMotor.setVelocity(1000);
-            }
-            else if (gamepad1.b) {
+            } else if (gamepad1.b) {
                 frontLeftMotor.setTargetPosition(-2000);
                 frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 frontLeftMotor.setVelocity(1000);
-            }
-            else if (gamepad1.x) {
+            } else if (gamepad1.x) {
                 frontLeftMotor.setTargetPosition(0);
                 frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
                 frontLeftMotor.setVelocity(1000);
-            }
-            else{
+            } else {
                 frontLeftMotor.setPower(0);
             }
 
