89e58b1e5284619f57334382adc4366c212fe35d
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-01-07 12:27:54 -0500

    Update2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jellyauto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Jellyauto.java
@@ -31,6 +31,8 @@ import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.firstinspires.ftc.teamcode.Framework.BaseOpMode;
+import org.firstinspires.ftc.teamcode.Framework.Slides;
+import org.firstinspires.ftc.teamcode.Framework.Claws;
 
 import java.util.ArrayList;
  
