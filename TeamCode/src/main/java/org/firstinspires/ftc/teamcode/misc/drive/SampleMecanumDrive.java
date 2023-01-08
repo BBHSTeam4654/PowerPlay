6a67dbe9ce1c7ce1a27c9e48fd44c01a9593fef3
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-01-08 14:07:27 -0500

    fix 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/drive/SampleMecanumDrive.java
@@ -20,6 +20,7 @@ import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityCons
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
