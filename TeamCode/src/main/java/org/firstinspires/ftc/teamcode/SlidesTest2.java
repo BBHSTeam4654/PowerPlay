79d64cc32fc4f2ba4dc789ecb459de3dc8d63474
matthewlaterza <102398918+matthewlaterza@users.noreply.github.com>
2023-01-11 14:47:33 -0500

    Update SlidesTest2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlidesTest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlidesTest2.java
@@ -4,11 +4,15 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "Slides Test2")
 public class SlidesTest2 extends LinearOpMode {
+    ElapsedTime timer = new ElapsedTime();
+    private double lastError = 0;
     @Override
     public void runOpMode() throws InterruptedException {
+
         waitForStart();
 
         DcMotorEx frontLeftMotor = null;
@@ -20,19 +24,29 @@ public class SlidesTest2 extends LinearOpMode {
         frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         double target = 0;
+        double integralSum = 0;
+        double Kp = 0.016;
+        double Ki = 0.01;
+        double Kd = 0.01;
         waitForStart();
 
 
         while (opModeIsActive()) {
-            double leftPosition = (double) (frontLeftMotor.getCurrentPosition());
-
-            double left_current_error = target - leftPosition;
-            double lkp = 0.016;
-            double lp = lkp * left_current_error;
             if(target>0) {
                 target = 0;
             }
-            frontLeftMotor.setPower(lp);
+
+            double leftPosition = (double) (frontLeftMotor.getCurrentPosition());
+
+            double left_error = target - leftPosition;
+            integralSum+=left_error*timer.seconds();
+            double derivative = (left_error-lastError)/timer.seconds();
+            lastError = left_error;
+            timer.reset();
+            double output = (left_error*Kp) + (integralSum*Ki)+(derivative*Kd);
+
+
+            frontLeftMotor.setPower(output);
             telemetry.addData("Target", target);
             telemetry.update();
             if (gamepad1.left_stick_y != 0) {
