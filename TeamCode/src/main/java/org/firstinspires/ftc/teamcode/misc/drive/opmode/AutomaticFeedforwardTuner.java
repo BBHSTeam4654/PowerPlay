aa1bc0468691e32a356a95c39276e723dd905f20
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-01-16 23:49:42 -0500

    Fixing files please don't touch even if you think you are being helpful cough cough*

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/drive/opmode/AutomaticFeedforwardTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/drive/opmode/AutomaticFeedforwardTuner.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.teamcode.misc.drive.opmode;
 
+import static org.firstinspires.ftc.teamcode.misc.drive.DriveConstants.MAX_RPM;
+import static org.firstinspires.ftc.teamcode.misc.drive.DriveConstants.RUN_USING_ENCODER;
+import static org.firstinspires.ftc.teamcode.misc.drive.DriveConstants.rpmToVelocity;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
@@ -12,7 +16,6 @@ import com.qualcomm.robotcore.util.RobotLog;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.internal.system.Misc;
 import org.firstinspires.ftc.teamcode.misc.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.misc.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.misc.util.LoggingUtil;
 import org.firstinspires.ftc.teamcode.misc.util.RegressionUtil;
 
@@ -37,7 +40,7 @@ public class AutomaticFeedforwardTuner extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        if (DriveConstants.RUN_USING_ENCODER) {
+        if (RUN_USING_ENCODER) {
             RobotLog.setGlobalErrorMsg("Feedforward constants usually don't need to be tuned " +
                     "when using the built-in drive motor velocity PID.");
         }
@@ -94,7 +97,7 @@ public class AutomaticFeedforwardTuner extends LinearOpMode {
         telemetry.addLine("Running...");
         telemetry.update();
 
-        double maxVel = DriveConstants.rpmToVelocity(DriveConstants.MAX_RPM);
+        double maxVel = rpmToVelocity(MAX_RPM);
         double finalVel = MAX_POWER * maxVel;
         double accel = (finalVel * finalVel) / (2.0 * DISTANCE);
         double rampTime = Math.sqrt(2.0 * DISTANCE / accel);
