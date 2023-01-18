577deec8c99b0db7556450f19ad12dd9728f3f0e
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-01-18 07:55:15 -0500

    yayy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.8898; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 1.0100524; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 13.54331; // in
 
     /*
@@ -55,14 +55,14 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.01876;
+    public static double kA = 0.0025;
+    public static double kStatic = 0.05334;
 
     public static double MAX_VEL = 50;
     public static double MAX_ACCEL = 50;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
-    public static double MAX_ANG_ACCEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = Math.toRadians(180);
+    public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
 
     public static double encoderTicksToInches(double ticks) {
