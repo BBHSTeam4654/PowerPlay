784e5497148c166f7a27163e09f1d904d8badfdc
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-01-20 09:39:22 -0500

    yey

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Framework/PoseStorage.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.Framework;
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-
-public class PoseStorage {
-    public static Pose2d startPose = new Pose2d(-35, -62, Math.toRadians(90));
-    public static Pose2d currentPose = new Pose2d();
-}
