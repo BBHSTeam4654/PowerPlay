b7a7af155005ad3c11c1a7dfaf01d0919e3ffb24
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-02-09 16:31:45 -0500

    Right

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JellyTele.java
@@ -36,7 +36,7 @@ public class JellyTele extends BaseOpMode {
         IMU imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
         // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
