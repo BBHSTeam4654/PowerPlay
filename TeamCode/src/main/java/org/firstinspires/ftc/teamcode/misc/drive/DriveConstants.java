499911fa4d5c8b04b82380ba20888a1e2a7d8b9d
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-03-04 05:11:11 -0500

    thonk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/misc/drive/DriveConstants.java
@@ -32,7 +32,7 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = false;
+    public static final boolean RUN_USING_ENCODER = true;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
             getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
 
@@ -45,7 +45,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.8898; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 1.05; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 15.35; // in
 
     /*
